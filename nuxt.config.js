ec5e3837e0654535778d05aa42166f9d45cff29a
Caio Chami <caio.chami18@gmail.com>
2022-08-06 16:54:42 -0300

    feat: Create the home property page.

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: "Masterting NuxtJS | %s",
+    titleTemplate: "Mastering NuxtJS | %s",
     htmlAttrs: {
       lang: "en",
     },
---
+++ pages/homes/_id.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="flex flex-col">
+    <div class="flex">
+      <img
+        v-for="(image, index) in home.images"
+        :src="image"
+        :key="index"
+        class="h-48 w-48"
+        alt="home image"
+      />
+    </div>
+    <div class="flex flex-col">
+      <h1 class="text-2xl font-bold">{{ home.title }}</h1>
+      <p class="text-lg">{{ home.description }}</p>
+      <span>${{ home.pricePerNight }} / Night</span>
+      <div>
+        <img
+          src="/images/marker.svg"
+          class="h-4 w-4 inline-block"
+          alt="worker"
+        />
+        <span>{{ fullAddress }}</span>
+      </div>
+      <div>
+        <img src="/images/star.svg" alt="star" class="h-4 w-4 inline-block" />
+        <span>{{ home.reviewValue }}</span>
+      </div>
+      <span
+        >{{ home.guests }} guests, {{ home.bedrooms }} rooms,
+        {{ home.beds }} beds, {{ home.bathrooms }} bath</span
+      >
+    </div>
+  </div>
+</template>
+
+<script>
+import homes from "~/data/homes";
+
+export default {
+  head() {
+    return {
+      title: this.home.title,
+    };
+  },
+  data: () => ({
+    home: {},
+  }),
+  created() {
+    const Home = homes.find((home) => home.objectID === this.$route.params.id);
+
+    this.home = Home;
+  },
+  computed: {
+    fullAddress() {
+      const { address, city, state, country } = this.home.location;
+
+      return `${address}, ${city}, ${state}, ${country}`;
+    },
+  },
+};
+</script>
